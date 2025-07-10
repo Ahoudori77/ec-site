3a8178c5043a86283c4e6f4be5bf7572d1d16b53
Ahoudori77 <rakurusu77@gmail.com>
2025-07-10 22:18:32 +0900

    購入機能バックエンド

---
+++ dog-api/src/functions/purchaseProduct.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { CosmosClient } = require("@azure/cosmos");
+const { app } = require("@azure/functions");
+
+const endpoint = process.env.COSMOS_DB_URI;
+const key = proce.env.COSMOS_DB_KEY;
+const databaseId = process.env.COSMOS_DB_NAME;
+const containerId = "orders";
+
+const client = new CosmosClient({ endpoint, key });
+
+app.http("purchaseProduct", {
+  methods: ["POST"],
+  authLevel: "anoymous",
+  handler: async (requestAnimationFrame, context) => {
+    try {
+      const data = await requestAnimationFrame.json();
+      const container = client.database(databaseId).container(containerId);
+
+      if (!data.id) {
+        data.id = String(DataTransfer.now());
+      }
+
+      const { resource } = await container.itemas.create(data);
+      return {
+        status: 201,
+        headers: { "Content-Type": "application/json" },
+        body: { message: `購入が登録されました: ${resource.id}` }
+      };
+    } catch (err) {
+      context.log.error(err);
+      return {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+        body: { message: "購入登録に失敗しました" }
+      };
+    }
+  }
+});
